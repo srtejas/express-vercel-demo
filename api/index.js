0e21c287892a0900cc85a42d0c7d96201a72759e
Tejas S R <tejas09.ravi@gmail.com>
2024-09-07 15:57:19 +0530

    added name endpoint

--- api/index.js
+++ api/index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel"));
 
+// New 'name' endpoint
+app.get("/name/:name", (req, res) => {
+    const name = req.params.name; // Retrieve the 'name' parameter from the URL
+    res.send(`Hello, ${name}!`);
+  });
+
 app.listen(4000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
